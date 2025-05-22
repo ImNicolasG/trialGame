2ba76842c403e8cab39262fcaae8a290426c5205
Nicolas G <nicog99@live.com>
2025-05-21 21:13:42 -0400

    Changed variable names

--- main.py
+++ main.py
@@ -12,20 +12,20 @@ orange = (202, 120, 66) # player
 
 # # # #Character# # # #
 
-object_size = 50
+CHAR_SIZE = 50
 
 # Start Location
 x = 50
 y = 200
-charW = 30
-charH = 80
-vel = 5 # Velocity
+CHAR_W = 30
+CHAR_H = 80
+CHAR_VEL = 3 # Velocity
 
 # Set width and height for window
-width, height = 800, 300
+SCREEN_WIDTH, SCREEN_HEIGHT = 800, 300
 
 # Create window
-screen = pygame.display.set_mode((width, height))
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 screen.fill(beige) # Screen color
 
 pygame.display.set_caption("Sidescroller Game") # Title
@@ -45,16 +45,16 @@ while running:
     keys = pygame.key.get_pressed() # checks keypress
     # for each key pressed, check if it is at the edge of window. If so, stop
     if keys[pygame.K_LEFT] and x > 0:
-        x -= vel
-    if keys[pygame.K_RIGHT] and x < 800 - charW:
-        x += vel
+        x -= CHAR_VEL
+    if keys[pygame.K_RIGHT] and x < 800 - CHAR_W:
+        x += CHAR_VEL
     if keys[pygame.K_UP] and y > 0:
-        y -= vel
-    if keys[pygame.K_DOWN] and y < 300 -charH:
-        y +=vel
+        y -= CHAR_VEL
+    if keys[pygame.K_DOWN] and y < 300 - CHAR_H:
+        y +=CHAR_VEL
 
     screen.fill(beige) # Resets screen with background color
-    pygame.draw.rect(screen, orange,(x, y, charW, charH), border_radius=5) # screen/color/x-location/y-location/width-of-object/height-of-object/outline.
+    pygame.draw.rect(screen, orange,(x, y, CHAR_W, CHAR_H), border_radius=5) # screen/color/x-location/y-location/width-of-object/height-of-object/outline.
     pygame.display.update()
     #pygame.display.flip() # Update display
 
